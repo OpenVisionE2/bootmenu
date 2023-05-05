a1f9cd5c40fd0011b1570b94c818ad01f6543577
persianpros <persianpros@yahoo.com>
2023-05-05 18:58:02 +0330

    Sort images

--- README.md
+++ README.md
@@ -13,20 +13,15 @@ There are a couple of ways to access the multiboot menu: <br />
 2. Copy the file STARTUP_RECOVERY from the partition block /dev/mmcblk0p4 and save it as STARTUP or STARTUP_ONCE <br />
 3. When the box has a STANDBY button on the frontpanel press and hold it while the receiver boot/starts. <br />
 
-* CAREFULL only use this option when it's necessary.
+* CAREFUL only use this option when it's necessary.
 
-The receiver can been update without select an image. <br />
+The receiver can be updated without selecting an image. <br />
 In the image zip you can move the file unforce_"machine".txt to the root of your usb disk and rename it to force_"machine".txt <br />
-The file force_"machine".txt should contain the zip filename which you want to install. This image will automatically be installed if you enter the Multiboot. <br />
+The file force_"machine".txt should contain the zip filename that you want to install. This image will automatically be installed if you enter the Multiboot. <br />
 When the zip file has the right structure it will be installed in the first linux partition. <br />
 
 * Expert
 
-It's possible to add extra image by manually add startup lines like STARTUP_LINUX_5, STARTUP_LINUX_6 <br />
+It's possible to add extra images by manually adding startup lines such as STARTUP_LINUX_5, STARTUP_LINUX_6 <br />
 Copy STARTUP_LINUX_1, 2, 3 or 4 to STARTUP_LINUX_5, 6 etc, and modify the 'rootsubdir' option in the bootargs. <br />
-The kernel will be shared with the linux 1, 2, 3 or 4 image, depending on which startup file you copied. <br />
-
-* OE-A
-
-It wasn't a rule that images must be Open Source to be able to put their names here.
-Suddenly some OE-A members decided to drop some images (Not even considering how much they contributed to OE-A and Open Source) with the label "Closed" and keep some really "Closed" ones.
+The kernel will be shared with the linux 1, 2, 3 or 4 images, depending on which startup file you copied. <br />
--- php/current.php
+++ php/current.php
@@ -1,51 +1,51 @@
-<?php
 
-printList();
 
-function getDirArray($Path="./",$Filter=".*",$Sorted="True")
-{
-	$handle=opendir($Path);
-	while ($file = readdir($handle)) {
-		if (is_file("$Path/$file") && preg_match("/$Filter/i", $file) && strpos($file, "recovery") === false) {
-			$dirArray[] = $file;
+
+
+
+
 		}
 	}
-	closedir($handle);
-	if (!is_array($dirArray)) {
-		return 0;
 	}
-	if (strcasecmp($Sorted,"True")) {
-		shuffle($dirArray);
-	} else {
-		sort($dirArray);
 	}
-	return $dirArray;
-}
-
-function printList() {
-
-	$Path="./";
-	$Folder="";
-	if ( isset($_GET['open']) ) {
-		$Folder=$_GET['open'];
-		$Path="./".$_GET['open']."/";
 	}
-
-	$FileList = getDirArray($Path,"_multi.zip$|_nand.zip$|_mmc.zip$|_usb.zip$");
-
-	if ($FileList) {
-		$Url=$Folder."/".end($FileList);
-		if (file_exists($Url)) {
-		header('Content-Description: File Transfer');
-		header('Content-Type: application/octet-stream');
-		header('Content-Disposition: attachment; filename="'.basename($Url).'"');
-		header('Expires: 0');
-		header('Cache-Control: must-revalidate');
-		header('Pragma: public');
-		header('Content-Length: ' . filesize($Url));
-		readfile($Url);
-		exit;
-}
 	}
-}
 ?>
+{
+}
+}
+}
+			$dirArray[] = $file;
+	$FileList = getDirArray($Path,"_multi.zip$|_nand.zip$|_mmc.zip$|_usb.zip$");
+	$Folder="";
+		$Folder=$_GET['open'];
+	$handle=opendir($Path);
+	$Path="./";
+		$Path="./".$_GET['open']."/";
+		$Url=$Folder."/".end($FileList);
+	closedir($handle);
+	} else {
+		exit;
+function getDirArray($Path="./",$Filter=".*",$Sorted="True")
+function printList() {
+		header('Cache-Control: must-revalidate');
+		header('Content-Description: File Transfer');
+		header('Content-Disposition: attachment; filename="'.basename($Url).'"');
+		header('Content-Length: ' . filesize($Url));
+		header('Content-Type: application/octet-stream');
+		header('Expires: 0');
+		header('Pragma: public');
+	if ($FileList) {
+		if (file_exists($Url)) {
+	if (!is_array($dirArray)) {
+		if (is_file("$Path/$file") && preg_match("/$Filter/i", $file) && strpos($file, "recovery") === false) {
+	if ( isset($_GET['open']) ) {
+	if (strcasecmp($Sorted,"True")) {
+<?php
+printList();
+		readfile($Url);
+	return $dirArray;
+		return 0;
+		shuffle($dirArray);
+		sort($dirArray);
+	while ($file = readdir($handle)) {
